f2b020e6310284f543b8b64ceee9cecd0388a9f4
rafael <rafaelcnunes@icloud.com>
2023-05-07 18:22:05 -0300

    added am partners description

--- src/components/atoms/AmPartnersDescription/AmPartnersDescription.vue
+++ src/components/atoms/AmPartnersDescription/AmPartnersDescription.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="am_partners_description" class="pr-16 pl-16 pt-6 pb-20">
-    <span>Partners</span>
+  <div id="am_partners_description" class="pr-16 pl-16 pt-6 pb-20 flex flex-col items-center gap-2">
+    <span class="font-mono text-emerald-400 font-medium uppercase tracking-widest">Partners</span>
+    <span class="font-serif font-semibold text-xl">Lorem Ipsum Dolor</span>
+    <span class="max-w-xs text-center">Lorem ipsum, dolor sit amet consectetur adipiscing elit.</span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AmPartnersDescription',
-  components: {
-    //
-  }
 }
 </script>
