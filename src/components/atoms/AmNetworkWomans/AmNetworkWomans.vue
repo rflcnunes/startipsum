125471715da22ee706c9b628f71356c1ef535436
rafael <rafaelcnunes@icloud.com>
2023-05-07 18:41:42 -0300

    added am network womans desktop

--- src/components/atoms/AmNetworkWomans/AmNetworkWomans.vue
+++ src/components/atoms/AmNetworkWomans/AmNetworkWomans.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="am_networking_womans" class="flex flex-col justify-center md:pr-16 md:pl-16 md:pt-6 md:pb-20">
-    <IconNetworkingWomans class="w-96 pl-4 pr-4 lg:w-full" />
-    <div id="am_networking_womans-description" class="flex flex-col items-start gap-2 pr-4 pl-4 pt-4 pb-8">
+  <div id="am_networking_womans" class="flex flex-col justify-center md:flex-row md:items-center md:pr-16 md:pl-16 md:pt-12 md:pb-20 md:gap-10">
+    <IconNetworkingWomans class="w-96 pl-4 pr-4 lg:min-w-fit" />
+    <div id="am_networking_womans-description" class="flex flex-col items-start md:w-2/4 gap-2 pr-4 pl-4 pt-4 pb-8">
       <span class="font-serif text-2xl">Lorem ipsum dolor sit amet consectetur</span>
       <span class="text-sm text-gray-600"
         >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, dolor
