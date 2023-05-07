982d7da2ae32b0ed391f5a34974365a311688b0f
rafael <rafaelcnunes@icloud.com>
2023-05-07 20:03:25 -0300

    added am footer

---
+++ src/components/molecules/AmFooterHome/AmFooterHome.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    id="am_footer_home"
+    class="w-full bg-neutral-800 text-white flex flex-col items-center justify-center pt-10 pb-10 md:flex-row md:justify-between md:pr-48 md:pl-48 md:pt-14 md:pb-10"
+  >
+    <div id="am_footer_home-icons" class="flex flex-row gap-5 mb-5">
+      <IconMiniFacebook />
+      <IconMiniInstagram />
+      <IconMiniTwitter />
+      <IconMiniPinterest />
+      <IconMiniWhatsApp />
+      <IconMiniYouTube />
+    </div>
+    <div>
+      <span>@ Startipsum, 2023. All rights reserved.</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconMiniFacebook from '../../icons/IconMiniFacebook.vue'
+import IconMiniInstagram from '../../icons/IconMiniInstagram.vue'
+import IconMiniPinterest from '../../icons/IconMiniPinterest.vue'
+import IconMiniTwitter from '../../icons/IconMiniTwitter.vue'
+import IconMiniWhatsApp from '../../icons/IconMiniWhatsApp.vue'
+import IconMiniYouTube from '../../icons/IconMiniYouTube.vue'
+
+export default {
+  name: 'AmFooterHome',
+  components: {
+    IconMiniFacebook,
+    IconMiniInstagram,
+    IconMiniTwitter,
+    IconMiniPinterest,
+    IconMiniWhatsApp,
+    IconMiniYouTube
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import AmHeaderHome from '@/components/molecules/AmHeaderHome/AmHeaderHome.vue'
 import AmBodyHome from '../components/molecules/AmBodyHome/AmBodyHome.vue';
+import AmFooterHome from '../components/molecules/AmFooterHome/AmFooterHome.vue';
 </script>
 
 <template>
   <main>
     <AmHeaderHome />
     <AmBodyHome />
+    <AmFooterHome />
   </main>
 </template>
