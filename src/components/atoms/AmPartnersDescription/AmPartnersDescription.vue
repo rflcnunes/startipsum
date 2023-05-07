3091ba3ee36d0a9a72e8161db05b78c3808176b3
rafael <rafaelcnunes@icloud.com>
2023-05-07 18:27:26 -0300

    adjustments css

--- src/components/atoms/AmPartnersDescription/AmPartnersDescription.vue
+++ src/components/atoms/AmPartnersDescription/AmPartnersDescription.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="am_partners_description" class="pr-16 pl-16 pt-6 pb-20 flex flex-col items-center gap-2">
+  <div id="am_partners_description" class="flex pb-10 flex-col items-center gap-2">
     <span class="font-mono text-emerald-400 font-medium uppercase tracking-widest">Partners</span>
-    <span class="font-serif font-semibold text-xl">Lorem Ipsum Dolor</span>
+    <span class="font-serif font-semibold md:text-xl">Lorem Ipsum Dolor</span>
     <span class="max-w-xs text-center">Lorem ipsum, dolor sit amet consectetur adipiscing elit.</span>
   </div>
 </template>
--- src/components/molecules/AmBodyHome/AmBodyHome.vue
+++ src/components/molecules/AmBodyHome/AmBodyHome.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="am_body_home" class="pr-16 pl-16 pt-6 pb-20">
+  <div id="am_body_home" class="pr-16 pl-16 pt-6 pb-20 flex flex-col justify-center items-center">
     <AmPartnersDescription />
     <AmCompanies />
+    <button class="bg-black text-white text-sm pl-6 pr-6 pb-1 pt-1 rounded-sm mt-10">Lean More</button>
   </div>
 </template>
 
